f33a5dedb7ece3717cab5b174509c6c52d17e424
Jordan Prince <jordanmikha10@gmail.com>
2024-08-28 01:11:04 +0200

    Update and rename MainGame.java to Game.java

---
+++ Game.java
@@ -0,0 +1,22 @@
+//Java code here
+package main;
+
+import javax.swing.JFrame;
+
+public class Game {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This line states that the window will only close when the close button is pressed by the user.
+        window.setResizable(false);
+        window.setTitle("Flappy bird");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); // This is so we can see the display
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+} 
+
